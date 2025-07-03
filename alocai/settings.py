dd29fb212170a67095cbab7c22d29f9363325cb8
RickyM7 <24703825+RickyM7@users.noreply.github.com>
2025-07-03 12:15:18 -0300

    cleanup: deletado comentário com chave antiga inválida

--- alocai/settings.py
+++ alocai/settings.py
@@ -36,9 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# secret do render = _(ci9_($9wn%kpl*$rt*fat=emj!^wq1051=2c4f%2&!di=*f$  
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG=1 no .env para desenvolvimento
